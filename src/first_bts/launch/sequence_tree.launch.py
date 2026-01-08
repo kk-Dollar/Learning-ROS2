674d8c09e57c32c1043420ff602319504bc9530d
Krishna Pandit <imh10019.23@bitmesra.ac.in>
2026-01-09 00:58:41 +0530

    sequence_behaviour_tree

---
+++ src/first_bts/launch/sequence_tree.launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    ld=LaunchDescription()
+
+    tree_xml_path=PathJoinSubstitution([FindPackageShare("first_bts"),"trees","sequence.xml"])
+    
+    bt_node= Node(
+        package="first_bts",
+        executable="sequence_tree",
+        output="screen",
+        parameters=[{"tree_xml_file":tree_xml_path}]
+    )
+    ld.add_action(bt_node)
+    return ld
---
+++ src/first_bts/src/Sequence.cpp
@@ -0,0 +1,73 @@
+#include "rclcpp/rclcpp.hpp"
+#include "behaviortree_cpp/bt_factory.h"
+#include "behaviortree_cpp/behavior_tree.h"
+#include "std_msgs/msg/string.hpp"
+#include <chrono>
+using namespace std::chrono_literals;
+
+class HelloNode: public BT::StatefulActionNode {
+    public:
+    HelloNode(const std::string& action_name , const BT::NodeConfig& config): BT::StatefulActionNode(action_name,config){}
+
+    BT::NodeStatus onStart()override
+    {
+        getInput<std::string>("msg",hello_msg_);
+
+        return BT::NodeStatus::RUNNING;
+    }
+    BT::NodeStatus onRunning()override
+    {
+        std::cout<<hello_msg_<<std::endl;
+        return BT::NodeStatus::SUCCESS;
+    }
+
+    void onHalted()override
+    {
+        return;
+    }
+
+    static BT::PortsList providedPorts()
+    {
+        return {BT::InputPort<std::string>("msg")};
+    }
+    private:
+    std::string hello_msg_;
+
+};
+
+class BTExecutor: public rclcpp::Node{
+    public:
+    BTExecutor(): Node("bt_executor")
+    {
+        init_btree();
+        timer_=this->create_wall_timer(0.5s,std::bind(&BTExecutor::tick_function,this));
+    }
+
+    private:
+    void init_btree()
+    { 
+        factory_.registerNodeType<HelloNode>("HelloNode1");
+        factory_.registerNodeType<HelloNode>("HelloNode2");
+        this->declare_parameter<std::string>("tree_xml_file","");
+        std::string tree_file;
+        this->get_parameter("tree_xml_file",tree_file);
+        tree_=factory_.createTreeFromFile(tree_file);
+
+    }
+    void tick_function()
+    {
+        tree_.tickOnce();
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    BT::Tree tree_;
+    BT::BehaviorTreeFactory factory_;
+};
+
+int main(int argc, char* argv[] )
+{
+    rclcpp::init(argc,argv);
+    auto node= std::make_shared<BTExecutor>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
